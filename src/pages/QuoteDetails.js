824ed2bb19748e3a8da23a6a68f46d4f5ae9e48e
Cruelkid <sonofammit@gmail.com>
2021-09-26 23:28:03 +0300

    added show comments button

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
 import { Route, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -20,6 +21,13 @@ const QuoteDetails = () => {
     return (
         <Fragment>
             <HighlightedQuote author={quote.author} text={quote.text} />
+            <Route path="/quotes/:qID" exact>
+                <div className="centered">
+                    <Link className="btn--flat" to={`/quotes/${params.qID}/comments`}>
+                        Show comments
+                    </Link>
+                </div>
+            </Route>
             <Route path="/quotes/:qID/comments">
                 <Comments />
             </Route>
