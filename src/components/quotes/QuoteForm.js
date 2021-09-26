92eee31bc1375d8452306c2a8f3b93670f610c43
Cruelkid <sonofammit@gmail.com>
2021-09-26 23:02:26 +0300

    added warning message when leaving a form, added react router history functionality

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,12 +1,15 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
+import { Prompt, useHistory } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import styles from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
     const authorInputRef = useRef();
     const textInputRef = useRef();
+    const [isEntering, setIsEntering] = useState(false);
+    const history = useHistory();
 
     function submitFormHandler(event) {
         event.preventDefault();
@@ -14,33 +17,59 @@ const QuoteForm = (props) => {
         const enteredAuthor = authorInputRef.current.value;
         const enteredText = textInputRef.current.value;
 
-        // optional: Could validate here
-
-        props.onAddQuote({ author: enteredAuthor, text: enteredText });
+        if (enteredAuthor.length > 1 && enteredText.length > 1) {
+            props.onAddQuote({ author: enteredAuthor, text: enteredText });
+        } else {
+            history.push('/quotes');
+        }
     }
 
-    return (
-        <Card>
-            <form className={classes.form} onSubmit={submitFormHandler}>
-                {props.isLoading && (
-                    <div className={classes.loading}>
-                        <LoadingSpinner />
-                    </div>
-                )}
+    const formFocusHandler = () => {
+        setIsEntering(true);
+    };
 
-                <div className={classes.control}>
-                    <label htmlFor="author">Author</label>
-                    <input type="text" id="author" ref={authorInputRef} />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor="text">Text</label>
-                    <textarea id="text" rows="5" ref={textInputRef}></textarea>
-                </div>
-                <div className={classes.actions}>
-                    <button className="btn">Add Quote</button>
-                </div>
-            </form>
-        </Card>
+    const finishEnteringHandler = () => {
+        setIsEntering(false);
+    };
+
+    return (
+        <Fragment>
+            <Prompt
+                when={isEntering}
+                message="Are you sure you want to leave?"
+            />
+            <Card>
+                <form
+                    className={styles.form}
+                    onFocus={formFocusHandler}
+                    onSubmit={submitFormHandler}
+                >
+                    {props.isLoading && (
+                        <div className={styles.loading}>
+                            <LoadingSpinner />
+                        </div>
+                    )}
+
+                    <div className={styles.control}>
+                        <label htmlFor="author">Author</label>
+                        <input type="text" id="author" ref={authorInputRef} />
+                    </div>
+                    <div className={styles.control}>
+                        <label htmlFor="text">Text</label>
+                        <textarea
+                            id="text"
+                            rows="5"
+                            ref={textInputRef}
+                        ></textarea>
+                    </div>
+                    <div className={styles.actions}>
+                        <button className="btn" onClick={finishEnteringHandler}>
+                            Add Quote
+                        </button>
+                    </div>
+                </form>
+            </Card>
+        </Fragment>
     );
 };
 
